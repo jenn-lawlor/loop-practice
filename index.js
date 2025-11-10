d1f4e2ffa299c48f04e182b998391593b078d186
deephealingmethod <151275307+deephealingmethod@users.noreply.github.com>
2025-11-09 15:16:05 -0900

    feat: set vars., write script in index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  para.textContent = "Output: ";
+  const num = input.value;
+  input.value = "";
+  input.focus();
+  for (i = 0; i <= num; i++) {
+    let sqRoot = Math.sqrt(i);
+    if (Math.floor(sqRoot) !== sqRoot) {
+      continue;
+    }
+    para.textContent += `${i}`;
+  }
+});
